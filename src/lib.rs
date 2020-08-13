f883aa7263351f00a1e9b0fd5f28f634b3addce3
Rui Loura <rui@joyent.com>
2020-08-13 16:12:52 -0400

    remove separate connection logic

--- src/lib.rs
+++ src/lib.rs
@@ -661,16 +661,19 @@ fn run_direct_db_shard_thread(
     let th_obj_tx = obj_tx.clone();
     let th_conf = conf.clone();
     let th_log = log.clone();
+    /*
     let mut conn_rt = tokio::runtime::Runtime::new().unwrap();
     let (client, connection) =
         conn_rt.block_on(directdb::get_db_connection(shard, conf, log))?;
+
+     */
 
     pool.execute(move || {
         let mut rt = tokio::runtime::Runtime::new().unwrap();
         if let Err(e) = rt.block_on(directdb::get_objects_from_shard(
             shard,
             th_conf,
-            Some((client, connection)),
+            None,
             th_log,
             th_obj_tx,
         )) {
